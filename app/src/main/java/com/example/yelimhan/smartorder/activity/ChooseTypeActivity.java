dbcfba17bb7fca09098220f9b9e7351de2b27f1f
Unknown <han4lim@naver.com>
2019-04-11 03:09:31 +0900

    자잘..

--- app/src/main/java/com/example/yelimhan/smartorder/activity/ChooseTypeActivity.java
+++ app/src/main/java/com/example/yelimhan/smartorder/activity/ChooseTypeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -26,6 +27,8 @@ public class ChooseTypeActivity extends AppCompatActivity implements ListAdapter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_choose_type);
         final OrderItem o = (OrderItem) getIntent().getSerializableExtra("Object");
         oData = (ArrayList<OrderItem>) getIntent().getSerializableExtra("menuList");
