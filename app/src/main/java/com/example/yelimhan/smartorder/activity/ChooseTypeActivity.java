3add6e6b373c6c81fab08d0516076c77f9a9e63b
Unknown <han4lim@naver.com>
2019-04-10 19:58:44 +0900

    겹치기시러

--- app/src/main/java/com/example/yelimhan/smartorder/activity/ChooseTypeActivity.java
+++ app/src/main/java/com/example/yelimhan/smartorder/activity/ChooseTypeActivity.java
@@ -3,6 +3,7 @@ package com.example.yelimhan.smartorder.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.yelimhan.smartorder.OrderItem;
 import com.example.yelimhan.smartorder.R;
 
 public class ChooseTypeActivity extends AppCompatActivity {
@@ -11,5 +12,7 @@ public class ChooseTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choos_type);
+        OrderItem o = (OrderItem) getIntent().getSerializableExtra("OBJECT");
+
     }
 }
