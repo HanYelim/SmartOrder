1c53e05d089136b891b3e90cc7eb65b469ae04df
Unknown <han4lim@naver.com>
2019-05-01 22:53:41 +0900

    품사 분류

--- app/src/main/java/com/example/yelimhan/smartorder/activity/VoiceActivity.java
+++ app/src/main/java/com/example/yelimhan/smartorder/activity/VoiceActivity.java
@@ -8,18 +8,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import com.example.yelimhan.smartorder.OrderItem;
 import com.example.yelimhan.smartorder.R;
 
 import org.snu.ids.kkma.index.Keyword;
 import org.snu.ids.kkma.index.KeywordExtractor;
 import org.snu.ids.kkma.index.KeywordList;
+import org.snu.ids.kkma.ma.MorphemeAnalyzer;
+import org.snu.ids.kkma.ma.Sentence;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class VoiceActivity extends AppCompatActivity {
     Intent i;
     TextView tv;
     SpeechRecognizer mRecognizer;
+    OrderItem o;
+    ArrayList<String> NNG.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,9 @@ public class VoiceActivity extends AppCompatActivity {
         mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         mRecognizer.setRecognitionListener(listener);
         mRecognizer.startListening(i);
+
+        MorphemeAnalyzer ma = new MorphemeAnalyzer();
+        ma.createLogger(null);
     }
 
     private RecognitionListener listener = new RecognitionListener() {
@@ -69,12 +79,34 @@ public class VoiceActivity extends AppCompatActivity {
             ArrayList<String> mResult = results.getStringArrayList(key);
             String[] rs = new String[mResult.size()];
             mResult.toArray(rs);
+//
+//            KeywordExtractor ke = new KeywordExtractor();
+//            KeywordList kl = ke.extractKeyword(rs[0], false);
+//            for (int i = 0; i < kl.size(); i++) {
+//                Keyword kwrd = kl.get(i);
+//                System.out.println(kwrd.getString() + "\t" + kwrd.getCnt());
+//
+//            }
 
-            KeywordExtractor ke = new KeywordExtractor();
-            KeywordList kl = ke.extractKeyword(rs[0], true);
-            for (int i = 0; i < kl.size(); i++) {
-                Keyword kwrd = kl.get(i);
-                System.out.println(kwrd.getString() + "\t" + kwrd.getCnt());
+            MorphemeAnalyzer ma = new MorphemeAnalyzer();
+            ma.createLogger(null);
+            try {
+                List ret = ma.analyze(rs[0]);
+                ret = ma.postProcess(ret);
+                ret = ma.leaveJustBest(ret);
+                List stl = ma.divideToSentences(ret);
+                for(int i = 0; i < stl.size(); i++){
+                    Sentence st = (Sentence) stl.get(i);
+                    for(int j = 0; j < st.size(); j++){
+                        //st.get(j).getExp() 따뜻하게
+                        tv.append(st.get(j).toString() + " " + st.get(j).getFirstMorp().getTag() + "\n");
+                        if(st.get(j).getFirstMorp().getTag().equals("NNG")){
+
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     };
